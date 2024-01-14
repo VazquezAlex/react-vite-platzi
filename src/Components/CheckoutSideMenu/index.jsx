3cfcb2b1ade72cdf97827b9270b1bddad61911da
VazquezAlex <alejandrovazquez.dev@gmail.com>
2024-01-13 20:44:57 -0600

    feature: created order card with data, and show it on card

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -5,10 +5,12 @@ import { XMarkIcon } from '@heroicons/react/24/solid';
 // Local imports.
 import { useShopiContext } from '../../Context';
 import './styles.css';
+import OrderCard from '../OrderCard';
 
 const CheckoutSideMenu = () => {
 
     const { 
+        cartProducts,
         isShoppingCartOpen,
         closeShoppingCart,
     } = useShopiContext();
@@ -28,7 +30,18 @@ const CheckoutSideMenu = () => {
                     <XMarkIcon className = 'h-6 w-6 text-black-600' />
                 </div>
             </div>
-            
+            <div className = 'px-6 flex flex-col gap-y-2'>
+                {
+                    cartProducts?.map(product => (
+                        <OrderCard 
+                            key = { product.id }
+                            title = { product.title }
+                            imageUrl = { product.image }
+                            price  = { product.price }
+                        />
+                    ))
+                }
+            </div>
         </aside>
     );
 }
